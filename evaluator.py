da4e72dcf22a90abd1c061c37d7323489e75e5af
JHurricane96 <arun.jhurricane@gmail.com>
2016-03-08 21:44:21 +0530

    Use tapered evaluation function

--- evaluator.py
+++ evaluator.py
@@ -20,9 +20,9 @@ import weights
 MAX_SCORE = 10000
 
 materialPnts = [[0, -10, -40, -41, -60, -120, 0], [0, 10, 40, 41, 60, 120, 0]]
+phasePnts = [0, 1, 1, 2, 4, 0]
 
 def evaluator(board, result):
-	#epd = board.epd()
 	pnts = 0
 
 	if not result == "*":
@@ -37,32 +37,45 @@ def evaluator(board, result):
 			pnts = -pnts
 		return pnts
 
-	i = 0
-	#To do: implement tapered evaluation function
-	endgameLerp = min(board.fullmove_number, 50)
+	matPnts = initPnts = endPnts = 0
+	phase = totalPhase = 24
+
 	for i in xrange(0, 64):
 		piece = board.piece_at(i)
 		if piece:
-			pnts += materialPnts[int(piece.color)][piece.piece_type]
-			#endgameLerp += materialPnts[0][piece.piece_type]
-			pnts += weights.initPosPnts[int(piece.color)][piece.piece_type-1][i]*(50-endgameLerp)/50
-			pnts += weights.finalPosPnts[int(piece.color)][piece.piece_type-1][i]*endgameLerp/50
+			matPnts += materialPnts[int(piece.color)][piece.piece_type]
+			initPnts += weights.initPosPnts[int(piece.color)][piece.piece_type-1][i]
+			endPnts += weights.finalPosPnts[int(piece.color)][piece.piece_type-1][i]
+			phase += phasePnts[piece.piece_type-1]
+
+	phase = (phase * 256 + (totalPhase / 2)) / totalPhase
+	pnts = (((matPnts + initPnts) * (256 - phase)) + ((matPnts + endPnts) * phase)) / 256
+
 	if (board.turn == False):
 		pnts = -pnts
 	return pnts
 
 def findFeatures(board, color):
+
 	featuresRawInit = [[[0 for i in range (0, 64)] for j in range (0, 6)] for k in range (0, 2)]
 	featuresRawFin = [[[0 for i in range (0, 64)] for j in range (0, 6)] for k in range (0, 2)]
 	featuresInit = []
 	featuresFin = []
-	endgameLerp = min(board.fullmove_number, 50)
 	colorType = 1 if color else -1
+	phase = totalPhase = 24
+
 	for i in xrange(0, 64):
 		piece = board.piece_at(i)
 		if piece:
-			featuresRawInit[int(piece.color)][piece.piece_type - 1][i] = (50.0 - endgameLerp) / 50.0 * colorType
-			featuresRawFin[int(piece.color)][piece.piece_type - 1][i] = endgameLerp / 50.0 * colorType
+			phase += phasePnts[piece.piece_type - 1]
+
+	phase = (phase * 256 + (totalPhase / 2)) / totalPhase
+
+	for i in xrange(0, 64):
+		piece = board.piece_at(i)
+		if piece:
+			featuresRawInit[int(piece.color)][piece.piece_type - 1][i] = (256.0 - phase) / 256.0 * colorType
+			featuresRawFin[int(piece.color)][piece.piece_type - 1][i] = phase / 256.0 * colorType
 
 	for k in range(2):
 		for j in range(6):
