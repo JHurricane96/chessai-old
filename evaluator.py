5e98663fefc5983a2a3fecd13df1a5dcd16abcd0
JHurricane96 <arun.jhurricane@gmail.com>
2016-03-09 17:31:02 +0530

    Correct phase in tapered eval

--- evaluator.py
+++ evaluator.py
@@ -1,25 +1,8 @@
 import chess
-#import weightsInit as weights
 import weights
+import config
 
-"""materialPnts = {
-	"p": -10,
-	"b": -41,
-	"n": -40,
-	"r": -60,
-	"q": -120,
-	"k": 0,
-	"P": +10,
-	"B": +41,
-	"N": +40,
-	"R": +60,
-	"Q": +120,
-	"K": 0
-}"""
-
-MAX_SCORE = 10000
-
-materialPnts = [[0, -10, -40, -41, -60, -120, 0], [0, 10, 40, 41, 60, 120, 0]]
+materialPnts = [[-10, -40, -41, -60, -120, 0], [10, 40, 41, 60, 120, 0]]
 phasePnts = [0, 1, 1, 2, 4, 0]
 
 def evaluator(board, result):
@@ -30,9 +13,9 @@ def evaluator(board, result):
 			if result[1] == "/":
 				pnts = 0
 			else:
-				pnts = MAX_SCORE
+				pnts = config.MAX_SCORE
 		else:
-			pnts = -MAX_SCORE
+			pnts = -config.MAX_SCORE
 		if (board.turn == False):
 			pnts = -pnts
 		return pnts
@@ -43,13 +26,14 @@ def evaluator(board, result):
 	for i in xrange(0, 64):
 		piece = board.piece_at(i)
 		if piece:
-			matPnts += materialPnts[int(piece.color)][piece.piece_type]
-			initPnts += weights.initPosPnts[int(piece.color)][piece.piece_type-1][i]
-			endPnts += weights.finalPosPnts[int(piece.color)][piece.piece_type-1][i]
-			phase += phasePnts[piece.piece_type-1]
+			matPnts += materialPnts[int(piece.color)][piece.piece_type - 1]
+			initPnts += weights.initPosPnts[int(piece.color)][piece.piece_type - 1][i]
+			endPnts += weights.finalPosPnts[int(piece.color)][piece.piece_type - 1][i]
+			phase -= phasePnts[piece.piece_type - 1]
 
 	phase = (phase * 256 + (totalPhase / 2)) / totalPhase
-	pnts = (((matPnts + initPnts) * (256 - phase)) + ((matPnts + endPnts) * phase)) / 256
+	#pnts = (((matPnts + initPnts) * (256 - phase)) + ((matPnts + endPnts) * phase)) / 256
+	pnts = matPnts + ((initPnts * (256 - phase)) + (endPnts * phase)) / 256
 
 	if (board.turn == False):
 		pnts = -pnts
@@ -67,7 +51,7 @@ def findFeatures(board, color):
 	for i in xrange(0, 64):
 		piece = board.piece_at(i)
 		if piece:
-			phase += phasePnts[piece.piece_type - 1]
+			phase -= phasePnts[piece.piece_type - 1]
 
 	phase = (phase * 256 + (totalPhase / 2)) / totalPhase
 
