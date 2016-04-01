a31732ebd2f06bc2871027d76a4aa91c84666727
JHurricane96 <arun.jhurricane@gmail.com>
2016-04-01 10:57:01 +0530

    Reduce learning rate decay

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ MAX_DEPTH = 4 # Maximum depth of negamax
 MAX_SCORE = 1000 # Maximum score that is awarded in static evaluation
 
 ALPHA_INIT = 0.002 # Initial learning rate
-ALPHA_DEC_FACTOR = 1.003 # Learning rate gets divided by this after every game
+ALPHA_DEC_FACTOR = 1.0001 # Learning rate gets divided by this after every game
 LAMBDA = 0.7 # Temporal decay factor
-MAX_GAMES = 10000 # Number of games to learn from
+MAX_GAMES = 5000 # Number of games to learn from
