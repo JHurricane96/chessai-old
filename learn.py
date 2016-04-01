b15bbca5696c36aafbe6671adf9207c90ce5f367
JHurricane96 <arun.jhurricane@gmail.com>
2016-04-01 10:57:40 +0530

    Learn from both black and white

--- learn.py
+++ learn.py
@@ -35,71 +35,83 @@ def main():
 			state = state.variation(0)
 
 		#Find winner
-		board = state.board()
 		whitePnt = game.headers["Result"][0]
 		if whitePnt == "1" and game.headers["Result"][1] != "/":
-			color = True
+			winColor = True
 		elif whitePnt == "0":
-			color = False
+			winColor = False
 		else:
 			continue
 
 		print "\nGame ", counter + 1
 
-		#Initialize stuff
-		scores = [config.MAX_SCORE]
-		boards = []
-		featuresInit = []
-		featuresFinal = []
-		boards.append(board.copy())
+		#Clear transposition tables
 		transTable.table.clear()
 		transTable.size = 0
-		turnCounter = 0
 
-		#Backprop through game, storing scores and features. Might as well do this in forward prop, will revise later
-		while not game.board() == board:
-			board.pop()
-			if board.turn == color:
-				#transTable.table.clear()
-				#transTable.size = 0
-				#guess = negamax.ABnegamax(board, config.MAX_DEPTH, 0, -(config.MAX_SCORE), (config.MAX_SCORE), transTable)[1]
-				#guess = negamax.mtd(board, config.MAX_DEPTH, config.MAX_SCORE, transTable, config.MAX_ITER_MTD)[1]
-				scores.append(negamax.mtdf(board, config.MAX_DEPTH, transTable, config.MAX_ITER_MTD)[1])
-				finalBoardPos = chess.Board(transTable.table[board.zobrist_hash()].finalBoardPos)
-				boards.append(finalBoardPos)
-				fI, fF = evaluator.findFeatures(finalBoardPos, color)
-				featuresInit.append(fI)
-				featuresFinal.append(fF)
+		for color in xrange(2):
 
-				turnCounter = turnCounter + 1
-				print "Turn ", turnCounter, '\r'
-		print '\n'
+			#Initialize stuff
+			board = state.board()
+			if winColor == color:
+				scores = [config.MAX_SCORE]
+			else:
+				scores = [-config.MAX_SCORE]
+			boards = []
+			featuresInit = []
+			featuresFinal = []
+			boards.append(board.copy())
+			turnCounter = 0
+			if color:
+				print "White"
+			else:
+				print "Black"
 
-		#Reverse lists, because I was too lazy to do stuff in forward prop
-		scores.reverse()
-		boards.reverse()
-		featuresInit.reverse()
-		featuresFinal.reverse()
+			#Backprop through game, storing scores and features. Might as well do this in forward prop, will revise later
+			while not game.board() == board:
+				board.pop()
+				if board.turn == color:
+					#transTable.table.clear()
+					#transTable.size = 0
+					#guess = negamax.ABnegamax(board, config.MAX_DEPTH, 0, -(config.MAX_SCORE), (config.MAX_SCORE), transTable)[1]
+					#guess = negamax.mtd(board, config.MAX_DEPTH, config.MAX_SCORE, transTable, config.MAX_ITER_MTD)[1]
+					scores.append(negamax.mtdf(board, config.MAX_DEPTH, transTable, config.MAX_ITER_MTD)[1])
+					finalBoardPos = chess.Board(transTable.table[board.zobrist_hash()].finalBoardPos)
+					boards.append(finalBoardPos)
+					fI, fF = evaluator.findFeatures(finalBoardPos, color)
+					featuresInit.append(fI)
+					featuresFinal.append(fF)
 
-		#Learn piece square tables
-		wInit, wFin = learn(wInit, wFin, featuresInit, featuresFinal, scores, learningRate, config.LAMBDA)
-		learningRate /= config.ALPHA_DEC_FACTOR
-		#Write piece square tables to file
-		f = open("weights.py", "w")
-		f.write("initPosPnts = " + str(wInit) + "\nfinalPosPnts = " + str(wFin))
-		f.close()
-		#Update piece square tables in memory
-		weights.initPosPnts = list(wInit)
-		weights.finalPosPnts = list(wFin)
+					turnCounter = turnCounter + 1
+					print "Turn ", turnCounter, '\r',
+			print '\n',
 
-		#Debug info
-		"""print scores
-		#print featuresInit[10]
-		#print featuresFinal[10]
-		print transTable.hits
-		print transTable.notHits
-		print transTable.size
-		#print wInit"""
+			#Reverse lists, because I was too lazy to do stuff in forward prop
+			scores.reverse()
+			boards.reverse()
+			featuresInit.reverse()
+			featuresFinal.reverse()
+
+			#Learn piece square tables
+			wInit, wFin = learn(wInit, wFin, featuresInit, featuresFinal, scores, learningRate, config.LAMBDA)
+			learningRate /= config.ALPHA_DEC_FACTOR
+			#Write piece square tables to file
+			f = open("weights.py", "w")
+			f.write("initPosPnts = " + str(wInit) + "\nfinalPosPnts = " + str(wFin))
+			f.close()
+			#Update piece square tables in memory
+			weights.initPosPnts = list(wInit)
+			weights.finalPosPnts = list(wFin)
+
+			#Debug info
+			"""print scores
+			#print featuresInit[10]
+			#print featuresFinal[10]
+			print transTable.hits
+			print transTable.notHits
+			print transTable.size
+			#print wInit"""
+
 		counter = counter + 1
 		if counter == config.MAX_GAMES:
 			break
@@ -130,7 +142,7 @@ def learn(wRawInit, wRawFin, fInit, fFinal, J, alpha, lambdaDecay):
 	#calculate update amount (with sign) for parameters	
 	updateMagInit = updateMagFinal = [0 for i in range(sizeW)]
 	for t in range(sizeJ - 1):
-		propTempDiff = 0 #propogated temporal difference
+		propTempDiff = 0 #propagated temporal difference
 		for j in range(t, sizeJ - 1):
 			propTempDiff += lambdaDecay**(j - t) * (J[j + 1] - J[j])
 		updateMagInit = [updateMagInit[i] + (propTempDiff * fInit[t][i]) for i in range(sizeW)]
