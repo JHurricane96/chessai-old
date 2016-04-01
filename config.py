5cf9eadc72a36614b67b93a2eca279d0b7551a7c
JHurricane96 <arun.jhurricane@gmail.com>
2016-04-01 17:05:46 +0530

    Tweak learning rate to suit common piece square tables

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ PAWN_SCORE = 100 # Material score of a single pawn
 MAX_POSITION_SCORE = PAWN_SCORE / 2 # Maximum score awarded in piece square tables
 MAX_SCORE = PAWN_SCORE * 100 # Maximum score that is awarded in static evaluation
 
-ALPHA_INIT = 0.002 # Initial learning rate
+ALPHA_INIT = 0.001 # Initial learning rate
 ALPHA_DEC_FACTOR = 1.0001 # Learning rate gets divided by this after every game
 LAMBDA = 0.7 # Temporal decay factor
 MAX_GAMES = 5000 # Number of games to learn from
